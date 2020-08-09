5cf1982b2f7de446ee8e72152c9dce91b9b38436
Alina-Goncalves <67426488+Alina-Goncalves@users.noreply.github.com>
2020-08-09 21:29:42 +0100

    Added new parameter to current position button & clean code response function

--- src/index.js
+++ src/index.js
@@ -69,37 +69,34 @@ let fahrenheit = document.querySelector("#getFahrenheit");
 fahrenheit.addEventListener("click", changeUnit);
 
 function getWeather(response) {
-  let nowTemp = Math.round(response.data.main.temp);
   let displayTemp = document.querySelector("#currentTemp");
-  displayTemp.innerHTML = `${nowTemp}`;
+  displayTemp.innerHTML = Math.round(response.data.main.temp);
 
-  let maxTemp = Math.round(response.data.main.temp_max);
   let displayMaxTemp = document.querySelector(".dayMaxTemperature");
-  displayMaxTemp.innerHTML = `${maxTemp}°`;
+  displayMaxTemp.innerHTML = Math.round(response.data.main.temp_max);
 
-  let minTemp = Math.round(response.data.main.temp_min);
   let displayMinTemp = document.querySelector(".dayMinTemperature");
-  displayMinTemp.innerHTML = `${minTemp}°`;
+  displayMinTemp.innerHTML = Math.round(response.data.main.temp_min);
 
-  let weatherDescription = response.data.weather[0].description;
   let displayDescription = document.querySelector(".weatherDescription");
-  displayDescription.innerHTML = `${weatherDescription}`;
+  displayDescription.innerHTML = response.data.weather[0].description;
 
-  let nowHumidity = response.data.main.humidity;
   let displayHumidity = document.querySelector("#humidity");
-  displayHumidity.innerHTML = `${nowHumidity} %`;
+  displayHumidity.innerHTML = `${response.data.main.humidity} %`;
 
-  let nowWind = Math.round(response.data.wind.speed * 3.6);
   let displayWind = document.querySelector("#wind");
-  displayWind.innerHTML = `${nowWind} km/h`;
+  displayWind.innerHTML = `${Math.round(response.data.wind.speed * 3.6)} km/h`;
+
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.name;
 }
 
-function showPosition(position) {
+function showPosition(position, city) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
 
   let nameCity = document.querySelector("#city");
-  nameCity.innerHTML = `Current Location`;
+  nameCity.innerHTML = `${city}`;
 
   let apiKey = "06b5f102b5d87678883f70debd49073e";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`;
